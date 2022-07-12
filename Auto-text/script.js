cc294d041130bd22435cc1ae5566c4fb483cc1b1
sumaya-kabir <sumayakabir4547@gmail.com>
2022-07-13 00:02:05 +0600

    auto text done

---
+++ Auto-text/script.js
@@ -0,0 +1,15 @@
+const text = "Always dream BIG so that you can achieve atleast the SMALL!";
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+
+    if (index > text.length) {
+        index = 0;
+    }
+}
+
+//setInterval(writeText, 100);
